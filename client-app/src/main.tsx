7b4177078a4af0f704df157943c15ebda09c9f8d
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-12-16 12:56:45 +0700

    End of section 5

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'semantic-ui-css/semantic.min.css'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "semantic-ui-css/semantic.min.css";
+import App from "./app/layout/App.tsx";
+import "./app/layout/style.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
