d6630efc59f3a12599abe643161052554f90f876
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-12-25 20:50:25 +0700

    End of section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from "../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../store/store";
+import { User, UserFromValues } from "../models/user";
 
 const sleep = (delay: number) => {
   return new Promise((reslove) => {
@@ -58,6 +59,12 @@ axios.interceptors.response.use(
 // The <T> notation in TypeScript is used to denote a generic type.
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
+axios.interceptors.request.use((config) => {
+  const token = store.commonStore.token;
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
 const request = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
   post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
@@ -75,8 +82,15 @@ const Activities = {
   delete: (id: string) => axios.delete<void>(`/activities/${id}`),
 };
 
+const Account = {
+  current: () => request.get<User>("/account"),
+  login: (user: UserFromValues) => request.post<User>("/account/login", user),
+  register: (user: UserFromValues) => request.post<User>("/account/register", user),
+};
+
 const agent = {
   Activities,
+  Account,
 };
 
 export default agent;
